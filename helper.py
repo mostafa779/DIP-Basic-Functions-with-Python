54bd608d4776c2f1fc0ab00033c6e6d3146eb845
Mostafa Ashraf <72988890+mostafa779@users.noreply.github.com>
2022-04-17 22:18:10 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -15,3 +15,58 @@ def toDecimal(bits,k):
         result += int(bits[i]) * (2 ** (7-i))
     
     return int(result)
+
+def replicate(image, n=3):
+    img = np.copy(image)
+    height = img.shape[0]
+    
+    newImg = []
+    
+    n = int(n/2)
+    
+    col1 = img[:,0].reshape(height,1,3)
+    col2 = img[:,-1].reshape(height,1,3)
+    
+    for i,row in enumerate(img):
+        newImg.append([])
+        
+        for _ in range(n+1):
+            newImg[i].append(col1[0][0])
+        
+        for pixel in row:
+            newImg[i].append(pixel)
+    
+        for _ in range(n+1):
+            newImg[i].append(col2[0][0])
+            
+    for _ in range(n+1):
+        newImg.insert(0,newImg[0])
+        newImg.insert(-1,newImg[-1])
+        
+    return np.array(newImg,dtype='uint8')
+
+
+def convlution_sum(window,matrix):
+    n = len(matrix)
+    
+    result = 0
+    for i in range(0,n):
+        for j in range(0,n):
+            result += matrix[i][j] * window[i][j]
+            
+    return result
+
+def getNeighbours(img, row, col, n):
+    matrix = np.zeros((n,n,3))
+    
+    k = int((n-1)/2)
+    
+    r1 = row-k
+    r2 = row+k+1
+    
+    c1 = col-k
+    c2 = col+k+1
+
+    matrix[:] = img[r1:r2,c1:c2]
+    
+    return matrix
